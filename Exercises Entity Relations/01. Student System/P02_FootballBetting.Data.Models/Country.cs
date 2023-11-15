1b75cde69c0029841cda6da9f55eb371ad64e022
aEnchev669 <aenchev669@abv.bg>
2023-11-15 22:53:04 +0200

    Create Country.cs

---
+++ Exercises Entity Relations/01. Student System/P02_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,28 @@
+﻿using P02_FootballBetting.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            this.Towns = new HashSet<Town>();
+        }
+        [Key]
+        public int CountryId { get; set; }
+        [Required]
+        [MaxLength(ValidationConstants.CountryNameMaxLenght)]
+        public string Name { get; set; } = null!;
+
+
+        public virtual ICollection<Town> Towns { get; set; } = null!;
+
+    }
+}
