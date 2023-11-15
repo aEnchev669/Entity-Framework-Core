887d4749c3f4638fb32211d3c33af5316cf9eb95
aEnchev669 <aenchev669@abv.bg>
2023-11-16 01:41:50 +0200

    Create PlayerStatistic.cs

---
+++ Exercises Entity Relations/01. Student System/P02_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class PlayerStatistic
+    {
+
+
+        [Required]
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+
+        public virtual Game Game { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Player))]
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; } = null!;
+
+        [Required]
+        public byte ScoredGoals { get; set; }
+        [Required]
+        public byte Assists { get; set; }
+        [Required]
+        public byte MinutesPlayed { get; set; }
+    }
+}
