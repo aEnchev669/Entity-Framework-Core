aedb94dcbe4509335091d4d6866caa4ca14cc2f8
aEnchev669 <aenchev669@abv.bg>
2023-11-15 22:53:00 +0200

    Create ValidationConstants.cs

---
+++ Exercises Entity Relations/01. Student System/P02_FootballBetting.Data.Common/ValidationConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Common
+{
+    public static class ValidationConstants
+    {
+        public const int TeamNameMaxLenght = 50;
+        public const int TeamLogoUrlMaxLenght = 2048;
+        public const int TeamInitialsMaxLenght = 3;
+        public const int ColorNameMaxLenght = 50;
+        public const int TownNameMaxLenght = 60;
+        public const int CountryNameMaxLenght = 60;
+        public const int PlayerNameMaxLenght = 50;
+        public const int PositionNameMaxLenght = 50;
+
+        public const int UsernameMaxLenght = 50;
+        public const int UserEmailMaxLenght = 50;
+        public const int UserPasswordMaxLenght = 50;
+        public const int UserNameMaxLenght = 50;
+
+        public const int GameResultMaxLenght = 8;
+
+    }
+}
