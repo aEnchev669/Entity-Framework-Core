334e30c574c4b8016d679b78902a043fb8417865
aEnchev669 <aenchev669@abv.bg>
2023-12-14 00:51:02 +0300

    Create Coach.cs

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        //Properties
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Nationality { get; set; }
+
+        //Collections
+        public virtual ICollection<Footballer> Footballers { get; set; } = new HashSet<Footballer>();
+    }
+}
+
+//Id – integer, Primary Key
+
+//· Name – text with length [2, 40] (required)
+
+//· Nationality – text (required)
+
+//· Footballers – collection of type Footballer
