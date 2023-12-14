36485193e50f2e5a0025b381170ce5880ff1a27e
aEnchev669 <aenchev669@abv.bg>
2023-12-15 02:29:35 +0300

    Create ExportCoacheDto.cs

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ExportDto/ExportCoacheDto.cs
@@ -0,0 +1,26 @@
+﻿using Footballers.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportCoacheDto
+    {
+        [XmlAttribute("FootballersCount")]
+        public int FootballersCount { get; set; }
+
+        [XmlElement("CoachName")]
+        public string CoachName { get; set; }
+
+        //Collections
+        [XmlArray("Footballers")]
+        public virtual ExportFootballerDto[] Footballers { get; set; }
+
+    }
+}
