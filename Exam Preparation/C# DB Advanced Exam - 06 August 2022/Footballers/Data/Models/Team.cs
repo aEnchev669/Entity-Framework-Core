753c57a4b42563c118134f2bf578d581fe5733a5
aEnchev669 <aenchev669@abv.bg>
2023-12-15 02:29:30 +0300

    Create Team.cs

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Team.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Footballers.Data.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Footballers.Data.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+       // [RegularExpression(@"^[a-zA-Z0-9 .-]*$")]
+        public string Name { get; set; }   =  null!;
+
+        [Required]
+        [MaxLength(40)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public int Trophies { get; set; }
+
+        public ICollection<TeamFootballer> TeamsFootballers { get; set; } = new HashSet<TeamFootballer>();
+    }
+}
+
+//Id – integer, Primary Key
+
+//· Name – text with length [3, 40].Should contain letters(lower and upper case), digits, spaces, a dot sign ('.') and a dash ('-'). (required)
+
+//· Nationality – text with length [2, 40] (required)
+
+//· Trophies – integer (required)
+
+//· TeamsFootballers – collection of type TeamFootballer
