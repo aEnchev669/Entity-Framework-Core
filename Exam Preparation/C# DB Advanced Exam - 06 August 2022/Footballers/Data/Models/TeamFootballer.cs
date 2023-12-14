d88142c81cfe843275e9e876523d06c477c82098
aEnchev669 <aenchev669@abv.bg>
2023-12-15 02:29:28 +0300

    Create TeamFootballer.cs

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/TeamFootballer.cs
@@ -0,0 +1,28 @@
+﻿using Footballers.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models
+{
+    public class TeamFootballer
+    {
+        [Required]
+        [ForeignKey(nameof(Team))]
+        public int TeamId { get; set; }
+        
+        public Team Team { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Footballer))]
+        public int FootballerId { get; set; }
+        public Footballer Footballer { get; set; } = null!;
+
+    }
+}
+//TeamId – integer, Primary Key, foreign key (required)
+
+//· Team – Team
+
+//· FootballerId – integer, Primary Key, foreign key (required)
+
+//· Footballer – Footballer
