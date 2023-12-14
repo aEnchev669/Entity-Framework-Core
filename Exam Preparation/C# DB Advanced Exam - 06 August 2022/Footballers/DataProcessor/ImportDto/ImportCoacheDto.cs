e75c397fbfba3f58e2cc2c8ccb23f4ef1e9ebfac
aEnchev669 <aenchev669@abv.bg>
2023-12-15 02:29:38 +0300

    Create ImportCoacheDto.cs

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportCoacheDto.cs
@@ -0,0 +1,28 @@
+﻿using Footballers.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportCoacheDto
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("Nationality")]
+        public string Nationality { get; set; }
+
+        [XmlArray("Footballers")]
+        public virtual ImportFootballerDto[] Footballers { get; set; } 
+    }
+}
