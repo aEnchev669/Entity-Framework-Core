f818d4970609c7deb89d15fabebb6b6cc1c3e1a9
aEnchev669 <aenchev669@abv.bg>
2023-12-28 00:21:15 +0300

    Create Play.cs

---
+++ Exam Preparation/Exam - 04 Dec-2021/Skeleton/Theatre/Data/Models/Play.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Theatre.Data.Models.Enums;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        //[MinLength(4)]
+        public string Title { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        //[Range(0.00, 10.00)]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        //[MinLength(4)]
+        public string Screenwriter { get; set; }
+
+        public ICollection<Cast> Casts { get; set; } = new HashSet<Cast>();
+        public ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+    }
+}
+
+//Id – integer, Primary Key 
+
+//Title – text with length [4, 50] (required)
+
+//Duration – TimeSpan in format {hours:minutes: seconds}, with a minimum length of 1 hour. (required) 
+
+//Rating – float in the range [0.00….10.00] (required)
+
+//Genre – enumeration of type Genre, with possible values (Drama, Comedy, Romance, Musical) (required)
+
+//Description – text with length up to 700 characters (required) 
+
+//Screenwriter – text with length [4, 30] (required)
+
+//Casts - a collection of type Cast 
+
+//Tickets - a collection of type Ticket 
