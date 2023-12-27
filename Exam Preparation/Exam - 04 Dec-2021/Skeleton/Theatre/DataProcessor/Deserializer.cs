bf5ff9f7a177fe9e34f4d922339810968bf8f2f7
aEnchev669 <aenchev669@abv.bg>
2023-12-28 00:21:33 +0300

    Create Deserializer.cs

---
+++ Exam Preparation/Exam - 04 Dec-2021/Skeleton/Theatre/DataProcessor/Deserializer.cs
@@ -0,0 +1,181 @@
+﻿namespace Theatre.DataProcessor
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Text;
+    using Theatre.Data;
+    using Theatre.Data.Models;
+    using Theatre.Data.Models.Enums;
+    using Theatre.DataProcessor.ImportDto;
+    using Theatre.Utilities;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfulImportPlay
+            = "Successfully imported {0} with genre {1} and a rating of {2}!";
+
+        private const string SuccessfulImportActor
+            = "Successfully imported actor {0} as a {1} character!";
+
+        private const string SuccessfulImportTheatre
+            = "Successfully imported theatre {0} with #{1} tickets!";
+
+        public static string ImportPlays(TheatreContext context, string xmlString)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+            StringBuilder sb = new StringBuilder();
+
+            ImportPlayDto[] playDtos = xmlHelper
+                .Deserialize<ImportPlayDto[]>(xmlString, "Plays");
+
+            ICollection<Play> validPlays = new HashSet<Play>();
+
+            foreach (var playDto in playDtos)
+            {
+
+                if (!IsValid(playDto) || string.IsNullOrEmpty(playDto.Screenwriter))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                if (!TimeSpan.TryParseExact(playDto.Duration, "c", CultureInfo.InvariantCulture, out TimeSpan duration))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                if (!Enum.TryParse<Genre>(playDto.Genre, out Genre genre))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                if (duration.Hours < 1)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Play play = new Play()
+                {
+                    Title = playDto.Title,
+                    Duration = duration,
+                    Rating = playDto.Rating,
+                    Genre = genre,
+                    Description = playDto.Description,
+                    Screenwriter = playDto.Screenwriter,
+                };
+
+                sb.AppendLine($"Successfully imported {playDto.Title} with genre {playDto.Genre} and a rating of {playDto.Rating}!");
+                validPlays.Add(play);
+            }
+
+            context.AddRange(validPlays);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportCasts(TheatreContext context, string xmlString)
+        {
+
+            XmlHelper xmlHelper = new XmlHelper();
+            StringBuilder sb = new StringBuilder();
+
+            ImportCastDto[] castDtos = xmlHelper
+                .Deserialize<ImportCastDto[]>(xmlString, "Casts");
+
+            ICollection<Cast> validCasts = new HashSet<Cast>();
+
+            foreach (var cast in castDtos)
+            {
+                if (!IsValid(cast))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Cast validCast = new Cast()
+                {
+                    FullName = cast.FullName,
+                    IsMainCharacter = cast.IsMainCharacter,
+                    PhoneNumber = cast.PhoneNumber,
+                    PlayId = cast.PlayId,
+                };
+
+                string mainOrLesser = (validCast.IsMainCharacter ? "main" : "lesser");
+
+                sb.AppendLine($"Successfully imported actor {validCast.FullName} as a {mainOrLesser} character!");
+
+                validCasts.Add(validCast);
+            }
+
+            context.AddRange(validCasts);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportTtheatersTickets(TheatreContext context, string jsonString)
+        {
+            ImportTheatreDto[] TheatreDtos = JsonConvert.DeserializeObject<ImportTheatreDto[]>(jsonString);
+
+            ICollection<Theatre> validTheatres = new HashSet<Theatre>();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var theatreDto in TheatreDtos)
+            {
+                if (!IsValid(theatreDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Theatre validTheatre = new Theatre()
+                {
+                    Name = theatreDto.Name,
+                    NumberOfHalls = theatreDto.NumberOfHalls,
+                    Director = theatreDto.Director,
+                };
+
+                foreach (var ticket in theatreDto.Tickets)
+                {
+                    if (!IsValid(ticket))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    validTheatre.Tickets.Add(new Ticket()
+                    {
+                        Price = ticket.Price,
+                        RowNumber = ticket.RowNumber,
+                        PlayId = ticket.PlayId
+                    });
+                }
+
+                validTheatres.Add(validTheatre);
+                sb.AppendLine($"Successfully imported theatre {validTheatre.Name} with #{validTheatre.Tickets.Count} tickets!");
+            }
+
+            context.AddRange(validTheatres);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        private static bool IsValid(object obj)
+        {
+            var validator = new ValidationContext(obj);
+            var validationRes = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(obj, validator, validationRes, true);
+            return result;
+        }
+    }
+}
