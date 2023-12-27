864b14e30445911265447857aed75bc0c192c172
aEnchev669 <aenchev669@abv.bg>
2023-12-28 00:21:26 +0300

    Create ImportCastDto.cs

---
+++ Exam Preparation/Exam - 04 Dec-2021/Skeleton/Theatre/DataProcessor/ImportDto/ImportCastDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Theatre.Data.Models;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Cast")]
+    public class ImportCastDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        [XmlElement("FullName")]
+        public string FullName { get; set; }
+
+        [Required]
+        [XmlElement("IsMainCharacter")]
+        public bool IsMainCharacter { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+        [XmlElement("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [XmlElement("PlayId")]
+        public int PlayId { get; set; }
+    }
+}
