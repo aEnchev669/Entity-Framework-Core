ac92fd7d1ea7b4e89c062f7d1582d9092fefec43
aEnchev669 <aenchev669@abv.bg>
2023-12-28 00:21:22 +0300

    Create Theatre.cs

---
+++ Exam Preparation/Exam - 04 Dec-2021/Skeleton/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+
+namespace Theatre.Data.Models
+{
+    public class Theatre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        //[MinLength(4)]
+        public string Name { get; set; }
+
+        [Required]
+        //[Range(1, 10)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        //[MinLength(4)]
+        public string Director { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+    }
+}
+
+//Id – integer, Primary Key 
+
+//Name – text with length [4, 30] (required)
+
+//NumberOfHalls – sbyte between[1…10] (required) 
+
+//Director – text with length [4, 30] (required)
+
+//Tickets - a collection of type Ticket 
